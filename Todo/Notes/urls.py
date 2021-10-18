b78b298cfe7c66f94aa14c40872ae4f73969ef07
rishabhDev2700 <rishavkumar2700@gmail.com>
2021-10-19 00:31:31 +0530

    created interface, add and delete function, user login.

--- Todo/Notes/urls.py
+++ Todo/Notes/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.create_user, name='create_user')
+    path('register/', views.create_user, name='create_user'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('add/', views.add_note, name='add'),
+    path('delete/<int:note_id>', views.delete, name='delete')
 ]
